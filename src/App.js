16eb3bd724bc197056c86c251505ecc8011ea5ab
kimdoyoung <rlaehdud5399@gamil.com>
2022-10-06 13:28:42 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
 
-function App() {
-  const [texts, setTexts] = useState([
-    {
-      id: 1,
-      title: "react를 배워봅시다.",
-    },
-  ]);
+function List({ texts }) {
+  return <div className="list">{texts.title}</div>;
+}
 
+function Form({ texts, setTexts }) {
   const [input, setInput] = useState({
     title: "",
   });
@@ -38,20 +35,32 @@ function App() {
       nextId.current += 1;
     }
   };
+  return (
+    <form className="form">
+      <input name="title" value={title} onChange={onChange} />
+      <button type="button" onClick={onCreate}>
+        추가하기
+      </button>
+    </form>
+  );
+}
+
+function App() {
+  const [texts, setTexts] = useState([
+    {
+      id: 1,
+      title: "react를 배워봅시다.",
+    },
+  ]);
 
   return (
     <div className="layout">
-      <form className="form">
-        <input name="title" value={title} onChange={onChange} />
-        <button type="button" onClick={onCreate}>
-          추가하기
-        </button>
-      </form>
+      <Form texts={texts} setTexts={setTexts} />
       <h1>Todo List</h1>
       <hr />
       <div className="warp">
         {texts.map((texts) => (
-          <div className="list">{texts.title}</div>
+          <List texts={texts} />
         ))}
       </div>
     </div>
